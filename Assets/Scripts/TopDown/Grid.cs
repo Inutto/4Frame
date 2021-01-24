fd7ccfc6ee8b04d03065b5b38f2f524ee419c721
Inutto <fushanwang@link.cuhk.edu.cn>
2021-01-24 19:43:33 +0800

    Add prepraed material for Topdown project(3D-Portal)

---
+++ Assets/Scripts/TopDown/Grid.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace FourFrame.TopDown
+{
+    public class Grid : MonoBehaviour
+    {
+        #region SINGLETON
+        public static Grid Instance;
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else
+            {
+                if (Instance != this)
+                {
+                    Destroy(gameObject);
+                }
+            }
+
+        }
+
+
+        #endregion SINGLETON
+
+        public Transform origin;
+        public Vector3 offset = new Vector3(0, 0, 0); // default
+        public float unit;
+
+
+        public LayerMask collLayer;
+        public LayerMask itemLayer;
+
+        public Vector2 Point2World(Point _pos)
+        {
+            var originPoint = origin.position + offset;
+            var newX = originPoint.x + _pos.x * unit;
+            var newY = originPoint.y + _pos.y * unit;
+            return new Vector2(newX, newY);
+        }
+
+    }
+
+    [System.Serializable]
+    public class Point
+    {
+        public int x;
+        public int y;
+
+        public Point()
+        {
+            x = 0;
+            y = 0;
+        }
+
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static Point operator +(Point p1, Point p2)
+        {
+            Point p = new Point();
+            p.x = p1.x + p2.x;
+            p.y = p1.y + p2.y;
+            return p;
+        }
+
+
+
+        public static Point operator -(Point p1, Point p2)
+        {
+            Point p = new Point();
+            p.x = p1.x - p2.x;
+            p.y = p1.y - p2.y;
+            return p;
+        }
+
+
+    }
+
+
+}
+
+
---
+++ Assets/Scripts/TopDown/Instance.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace FourFrame.TopDown{
+
+    public class Instance : MonoBehaviour
+    {
+
+
+        #region SUPER PROPERTY
+
+        /// <summary>
+        /// the Point position of Instance
+        /// </summary>
+        [SerializeField] private Point _position;
+        public Point position
+        {
+            get
+            {
+                return _position;
+            }
+            set
+            {
+                _position = value;
+                SyncPosition();
+            }
+        }
+
+
+        /// <summary>
+        /// READ to complete timelineInput and WRITE to record onto timeline
+        /// </summary>
+        enum TimelineState { READ, WRITE }
+        TimelineState state = TimelineState.READ;
+
+
+
+        /// <summary>
+        /// Using Command Mode to Update Infor to Timeline
+        /// </summary>
+        public event Action OnCommand;
+
+
+
+
+
+        #endregion
+
+
+        #region SUPER METHOD
+
+        /// <summary>
+        /// Use this Method to move the Instance to adjacent Point
+        /// </summary>
+        public virtual void Move()
+        {
+
+        }
+
+        /// <summary>
+        /// Use this Method to move the Instance freely
+        /// </summary>
+        public virtual void Teleport()
+        {
+
+        }
+
+        #endregion
+
+
+        #region TOOLS
+
+        #endregion
+
+        #region PRIVATE IMPL
+
+        #endregion
+
+
+
+
+
+    }
+
+}
+
+
+
+
