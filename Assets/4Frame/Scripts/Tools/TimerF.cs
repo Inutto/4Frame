9c54cd8c2e7cbb420692077bbc21b64b4118ba0c
Inutto <57230511+Inutto@users.noreply.github.com>
2022-05-12 16:55:48 -0600

    Rename Again

--- Assets/4Frame/Scripts/Tools/TimerF.cs
+++ Assets/4Frame/Scripts/Tools/TimerF.cs
@@ -2,13 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestManager : MonoSingleton<TestManager>
+public class TimerF : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("This is a normal message.");
-        DebugF.Log("This is a DebugF Message!", gameObject);
         
     }
 
